5b729df45aa3ad202f0b62ebb46ea8ecb65a4c0a
Justin <ju3tin@hotmail.co.uk>
2020-11-27 09:28:46 +0000

    xc

---
+++ project/README.md
@@ -0,0 +1,1 @@
+Written for https://forum.jquery.com/topic/download-a-file-via-ajax#14737000005999144
---
+++ project/script.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("button").click(function() {
+    $.ajax({
+      url: "https://bemyfr1end.herokuapp.com/test2.wav",
+      type: "GET",
+      dataType: 'binary',
+      success: function(result) {
+        var url = URL.createObjectURL(result);
+        var $a = $('<a />', {
+          'href': url,
+          'download': 'document.pdf',
+          'text': "click"
+        }).hide().appendTo("body")[0].click();
+        setTimeout(function() {
+          URL.revokeObjectURL(url);
+        }, 10000);
+      }
+    });
+  });
+});
